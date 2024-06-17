e4240dbe6833a7906a23e8d629f94b7a67167ab4
Bogatov Oleg <oleg.ferari2015@yandex.ru>
2024-06-17 21:18:28 +0300

    add tryCatch, add ENCODE and DECODE

---
+++ src/main/java/ru/javarush/bogatov/cryptoanalizer/controllers/Actions.java
@@ -0,0 +1,27 @@
+package ru.javarush.bogatov.cryptoanalizer.controllers;
+
+import ru.javarush.bogatov.cryptoanalizer.commands.Action;
+import ru.javarush.bogatov.cryptoanalizer.commands.Decoder;
+import ru.javarush.bogatov.cryptoanalizer.commands.Encoder;
+import ru.javarush.bogatov.cryptoanalizer.exception.AppException;
+
+public enum Actions {
+    ENCODE(new Encoder()),
+    DECODE(new Decoder());
+
+    private final Action action;
+
+    Actions(Action action) {
+        this.action = action;
+    }
+
+    public static Action find(String actionName){
+        try {
+            Actions value = Actions.valueOf(actionName.toUpperCase());
+            Action action = value.action;
+            return action;
+        } catch (IllegalArgumentException e) {
+            throw new AppException();
+        }
+    }
+}
