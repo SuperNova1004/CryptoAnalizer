7b2b635acfe9cc73461bfc17b4e9c20347de612f
Bogatov Oleg <oleg.ferari2015@yandex.ru>
2024-06-17 19:56:23 +0300

    test commit

--- src/main/java/ru/javarush/bogatov/cryptoanalizer/Runner.java
+++ src/main/java/ru/javarush/bogatov/cryptoanalizer/Runner.java
@@ -2,6 +2,6 @@ package ru.javarush.bogatov.cryptoanalizer;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven");
+        System.out.println("Hello, Maven!");
     }
 }
