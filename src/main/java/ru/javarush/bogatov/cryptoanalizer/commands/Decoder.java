f5803f9ea6f3f425592287fa31da090b482f2ab5
Bogatov Oleg <oleg.ferari2015@yandex.ru>
2024-06-20 17:59:40 +0300

    no ideas for appexcept

--- src/main/java/ru/javarush/bogatov/cryptoanalizer/commands/Decoder.java
+++ src/main/java/ru/javarush/bogatov/cryptoanalizer/commands/Decoder.java
@@ -1,12 +1,14 @@
 package ru.javarush.bogatov.cryptoanalizer.commands;
 
 import ru.javarush.bogatov.cryptoanalizer.entity.Result;
+import ru.javarush.bogatov.cryptoanalizer.entity.ResultCode;
 
 public class Decoder implements Action {
 
 
     @Override
     public Result execute(String[] parameters) {
-        return null;
+        // TODO something do
+        return new Result("decode all right", ResultCode.OK);
     }
 }
--- src/main/java/ru/javarush/bogatov/cryptoanalizer/commands/Encoder.java
+++ src/main/java/ru/javarush/bogatov/cryptoanalizer/commands/Encoder.java
@@ -1,12 +1,14 @@
 package ru.javarush.bogatov.cryptoanalizer.commands;
 
 import ru.javarush.bogatov.cryptoanalizer.entity.Result;
+import ru.javarush.bogatov.cryptoanalizer.entity.ResultCode;
 
 public class Encoder implements Action {
 
 
     @Override
     public Result execute(String[] parameters) {
-        return null;
+        // TODO something do
+        return new Result("encode all right", ResultCode.OK);
     }
 }
